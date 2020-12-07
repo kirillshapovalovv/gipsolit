c4f0b79d21a29855b9e97d7062b4d7378c461ade
kirillshapovalovv <72507173+kirillshapovalovv@users.noreply.github.com>
2020-12-07 10:08:16 +0200

    added latest features

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    // Модальное окно Получить консультацию
+    $('.popup-btn').click(function () {
+      $('.popup').fadeIn(500)
+    });
+    $('.popup-close').click(function () {
+      $('.popup').fadeOut(500)
+    });
+
+
+    // Слайдер с отзывами
+    $('.feedback-slider').slick({
+      prevArrow: '<button type="button" class="feedback-slider-btn feedback-prev-btn"><img src="img/feedback/prewArrow.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="feedback-slider-btn feedback-next-btn"><img src="img/feedback/nextArrow.svg" alt=""></button>'
+    });
+
+
+
+    // Слайдер с преимуществами
+    $('.feature-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 2,
+                prevArrow: '<button class="prev arrow"></button>',
+                nextArrow: '<button class="next arrow"></button>',
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+                prevArrow: '<button class="prev arrow"></button>',
+                nextArrow: '<button class="next arrow"></button>',
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+});
+              
